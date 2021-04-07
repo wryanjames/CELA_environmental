1ceac9ff7d5c8b1a5bb1bdb9eab525d0ce2ebbb5
wryanjames <wrjames14@gmail.com>
2021-04-07 11:34:57 -0400

    convert water quality to csv and create data wrangling script

---
+++ CELAenv_dataClean.R
@@ -0,0 +1,7 @@
+#' """ CELA environmental data cleaning and wrangling
+#'     @author: Ryan James and Josh Linenfelser
+#'     date: 4/7/2021"""
+
+library(tidyverse)
+
+cody = read_csv('data/CELA_envTow.csv')
